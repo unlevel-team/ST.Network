d1a7b552670c191b0697e40d7bbe46d7950a5a9f
UnlevelTeam <nobody@nowhere.waw.net>
2016-05-23 19:40:47 +0200

    Working on package.json file...
    - Added toES5 for distribution facilities

---
+++ lib_es5/stNetwork/STNetwork.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/**
+ * SomeThings Network library
+ * 
+ * 
+ */
+
+var dataChannel_Lib = require("./DataChannel.js");
+var DC_SocketIO_Lib = require("./DC_SocketIO.js");
+var COMSystem_Lib = require("./COMSystem.js");
+
+var stNetwork_Lib = {
+
+	"DataMessage": dataChannel_Lib.DataMessage,
+	"DataChannel": dataChannel_Lib.DataChannel,
+	"DataChannelsManager": dataChannel_Lib.DataChannelsManager,
+
+	"DC_SocketIO": DC_SocketIO_Lib.DC_SocketIO,
+	"COMSystem": COMSystem_Lib.COMSystem
+
+};
+
+module.exports = stNetwork_Lib;
+//# sourceMappingURL=STNetwork.js.map
